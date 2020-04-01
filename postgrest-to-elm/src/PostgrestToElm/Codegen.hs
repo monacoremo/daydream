e44be141d997d3ffcea5cb624534c31a15f636ec
Remo <remo>
2020-04-01 09:06:33 +0200

    .

--- postgrest-to-elm/src/PostgrestToElm/Codegen.hs
+++ postgrest-to-elm/src/PostgrestToElm/Codegen.hs
@@ -5,32 +5,19 @@ module PostgrestToElm.Codegen
     ) where
 
 import Data.Text (Text)
-import qualified Data.Text as Text
-import GHC.Generics (Generic)
-import PostgREST.DbStructure (getDbStructure, getPgVersion)
-import System.Environment (getArgs)
-import GHC.Int (Int32)
-import System.Directory (createDirectoryIfMissing)
-import qualified Data.Aeson as Aeson
-import qualified Data.Aeson.Encode.Pretty as Aeson
-import qualified Data.ByteString.Char8
-import qualified Data.ByteString.Lazy.Char8
-import qualified Data.Text
-import qualified Hasql.Connection as Connection
-import qualified Hasql.Session as Session
-import qualified Hasql.Transaction.Sessions as HT
-import qualified PostgREST.Types as PostgREST
-import qualified Data.Maybe as Maybe
+import Data.Text.Manipulate (toPascal)
+import Data.Text.Prettyprint.Doc (Doc)
+import Data.Void (vacuous)
+import qualified Bound
 import qualified Data.HashMap.Strict as HashMap
+import qualified Data.Maybe as Maybe
+import qualified Language.Elm.Definition as Elm
+import qualified Language.Elm.Expression as Elm
 import qualified Language.Elm.Name as Elm
 import qualified Language.Elm.Pretty as Elm
 import qualified Language.Elm.Simplification as Elm
-import qualified Language.Elm.Definition as Elm
 import qualified Language.Elm.Type as ElmType
-import qualified Language.Elm.Expression as Elm
-import Data.Text.Prettyprint.Doc (Doc)
-import qualified Bound
-import Data.Void (vacuous)
+
 
 import PostgrestToElm.ApiStructure (ApiStructure (..), ApiTable (..))
 
@@ -54,11 +41,14 @@ apiTableToElm :: Elm.Module -> ApiTable -> [Elm.Definition]
 apiTableToElm rootModuleName apiTable =
     let
         moduleName =
-            rootModuleName ++ [apiTableName apiTable]
+            rootModuleName ++ [toPascal $ apiTableName apiTable]
+
+        qualified =
+            Elm.Qualified moduleName
     in
     [ Elm.Constant
-        (Elm.Qualified moduleName "get")
+        (qualified "get")
         0
-        (Bound.toScope $ vacuous $ ElmType.Global (Elm.Qualified moduleName "Test"))
+        (Bound.toScope $ vacuous $ ElmType.Global (qualified "Test"))
         (Elm.List [])
     ]
