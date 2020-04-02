880b8cd5e2380319eabb9f6627b1fc50959b4f9c
Remo <remo>
2020-04-02 21:25:35 +0200

    add plperl to Postgres build

--- deploy/overrides.nix
+++ deploy/overrides.nix
@@ -1,27 +1,37 @@
 pkgs:
 
 rec {
-  haskellPackages = pkgs.haskellPackages.override {
-    overrides = newHaskellPkgs: oldHaskellPkgs: rec {
-      servant-to-elm =
-        newHaskellPkgs.callPackage haskell/servant-to-elm.nix {};
+  haskellPackages =
+    pkgs.haskellPackages.override {
+      overrides = newHaskellPkgs: oldHaskellPkgs: rec {
+        servant-to-elm =
+          newHaskellPkgs.callPackage haskell/servant-to-elm.nix {};
 
-      haskell-to-elm =
-        newHaskellPkgs.callPackage haskell/haskell-to-elm.nix
-          { inherit elm-syntax; };
+        haskell-to-elm =
+          newHaskellPkgs.callPackage haskell/haskell-to-elm.nix
+            { inherit elm-syntax; };
 
-      elm-syntax =
-        newHaskellPkgs.callPackage haskell/elm-syntax.nix {};
+        elm-syntax =
+          newHaskellPkgs.callPackage haskell/elm-syntax.nix {};
 
-      postgrest =
-        newHaskellPkgs.callPackage haskell/postgrest.nix
-          { inherit hasql-pool configurator-pg; };
+        postgrest =
+          newHaskellPkgs.callPackage haskell/postgrest.nix
+            { inherit hasql-pool configurator-pg; };
 
-      hasql-pool =
-        newHaskellPkgs.callPackage haskell/hasql-pool.nix {};
+        hasql-pool =
+          newHaskellPkgs.callPackage haskell/hasql-pool.nix {};
 
-      configurator-pg =
-        newHaskellPkgs.callPackage haskell/configurator-pg.nix {};
+        configurator-pg =
+          newHaskellPkgs.callPackage haskell/configurator-pg.nix {};
+      };
     };
-  };
+
+  postgresql_12 =
+    pkgs.postgresql_12.overrideAttrs (attrs: {
+      configureFlags =
+        attrs.configureFlags ++ [ "--with-perl" ];
+
+      buildInputs =
+        attrs.buildInputs ++ [ pkgs.perl ];
+    });
 }
